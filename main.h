e67dbc4c917c98b9c4603885b322f734c3c4bb40
Konyancha1 <kmuua791@gmail.com>
2021-10-03 22:53:22 +0200

    Handle conversion specifiers c, s and %

--- main.h
+++ main.h
@@ -2,7 +2,13 @@
 #include main_h
 
 #include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int _printf(const char *format, ...);
+int _putchar(char c);
+int print_string(va_list ap);
+int print_char(va_list s);
+int print_percentage(void);
 
 #endif
---
+++ print_char.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_char - writes the character c to stdour
+ * @s: The character to print
+ * 
+ * Return: 1
+ */
+int print_char(va_list s)
+{
+unsigned char a;
+
+ a = va_arg(s, int);
+ _putchar(a);
+ return(1);
+}
+/**
+ * print_percentage - %
+ * 
+ * Return: 1
+ */
+int print_percentage(void)
+{
+_putchar('%');
+return (1);
+}
