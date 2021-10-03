c3581cc66122e1e5b02acbe70c8ec894b39d8a19
Konyancha1 <kmuua791@gmail.com>
2021-10-03 23:28:20 +0200

    Handle conversion specifiers c, s and %

--- main.h
+++ main.h
@@ -10,5 +10,16 @@ int _putchar(char c);
 int print_string(va_list ap);
 int print_char(va_list s);
 int print_percentage(void);
+/**
+ * struct code_format - Struct format
+ *
+ * @sc: The specifiers
+ * @f: The function associated
+ */
+typedef struct code_format
+{
+  char *sc;
+  int (*f)(va_list);
+} code_f;
 
 #endif
