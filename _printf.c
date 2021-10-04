ffc92868a82545cb932c36184c9abcdacc185574
Konyancha1 <kmuua791@gmail.com>
2021-10-04 23:32:18 +0200

    Use a local buffer of 1024 chars in order to call write as little as possible.

--- _printf.c
+++ _printf.c
@@ -12,32 +12,38 @@ va_list arguments;
 int (*function)(va_list, char *, unsigned int);
 char *buffer;
 
-va_start(arguments, format);
-buffer = malloc(sizeof(char) * 1024);
-while (format && format[i])
+va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
+if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+return (-1);
+if (!format[i])
+return (0);
+for (i = 0; format && format[i]; i++)
 {
 if (format[i] == '%')
 {
 if (format[i + 1] == '%')
 handl_buf(buffer, format[i], ibuf), len++;
 else if (format[i + 1] == '\0')
-{if (ibuf != 0)
-print_buf(buffer, ibuf);
-free(buffer);
+{
+print_buf(buffer, ibuf), free(buffer), va_end(arguments);
 return (-1);
 }
 else
 {
-function = get_print_func(format, i + 1);
-len += function(arguments, buffer, ibuf);
+function = get_print_func(format[i + 1]);
+if (function == NULL)
+{
+handl_buf(buffer, format[i], ibuf);
+len++, i--;
 }
-i++;
+else
+len += function(arguments, buffer, ibuf);
+} i++;
 }
 else
 handl_buf(buffer, format[i], ibuf), len++;
-i++, ibuf = len;
-while (ibuf > 1024)
-ibuf -= 1024;
+for (ibuf = len; ibuf > 1024; ibuf -= 1024)
+;
 }
 print_buf(buffer, ibuf), free(buffer), va_end(arguments);
 return (len);
