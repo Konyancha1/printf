e53a8b8ff2bda449ec888989eaf98a7a003a12d8
Ikudehinbu Beauty <86835129+mzbhewtee@users.noreply.github.com>
2021-10-05 14:47:20 +0200

    Update print_usr.c

--- print_usr.c
+++ print_usr.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * print_usr - writes the character c to stdout
- * @arguments: input string
- * @buf: buffer pointer
- * @ibuf: index for buffer pointer
- * Return: number of chars printed
- */
+* print_usr - writes the character c to stdout
+* @arguments: input string
+* @buf: buffer pointer
+* @ibuf: index for buffer pointer
+* Return: number of chars printed
+*/
 int print_usr(va_list arguments, char *buf, unsigned int ibuf)
 {
 unsigned char *str;
@@ -28,6 +28,7 @@ ibuf = handl_buf(buf, hexadecimal[6], ibuf);
 ibuf = handl_buf(buf, hexadecimal[7], ibuf);
 sum += 3;
 }
+
 else
 ibuf = handl_buf(buf, str[i], ibuf);
 }
